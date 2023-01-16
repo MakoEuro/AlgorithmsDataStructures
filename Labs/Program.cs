3d7b4e1e892d4afe0916af2a858d6b53a6e9f68e
Mako <starykovskyeric@gmail.com>
2023-01-16 08:52:47 -0600

    Add project files.

---
+++ Labs/Program.cs
@@ -0,0 +1,16 @@
+﻿// Enter num for words
+Console.WriteLine("Program for counting characters.");
+Console.WriteLine("Enter a number (for words)");
+int count = Int32.Parse(Console.ReadLine());
+
+// Enter words
+Console.WriteLine("Enter " + count + " words");
+string[] words = new string[count-1];
+for (int i = 0; i <= count; i++)
+{
+    words[i] = Console.ReadLine(); 
+}
+
+Console.WriteLine("Now type in a letter for the program to search and count: ");
+
+
