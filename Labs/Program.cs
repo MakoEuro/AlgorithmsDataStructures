fdba3aafd6e12a1c70d86302ce818f2717d47a24
Mako <starykovskyeric@gmail.com>
2023-01-17 00:26:56 -0600

    Finished lab2 entirely, all code is hardcoded.

--- Labs/Program.cs
+++ Labs/Program.cs
@@ -1,16 +1,47 @@
-﻿// Enter num for words
-Console.WriteLine("Program for counting characters.");
-Console.WriteLine("Enter a number (for words)");
-int count = Int32.Parse(Console.ReadLine());
+﻿// A program that produces an array of all of the characters that appear more than once in a string.
 
-// Enter words
-Console.WriteLine("Enter " + count + " words");
-string[] words = new string[count-1];
-for (int i = 0; i <= count; i++)
+using System.Collections.Generic;
+
+string input = "Programmatic Python".ToLower();
+List<char> dupe = new List<char>();
+foreach (var item in input)
 {
-    words[i] = Console.ReadLine(); 
+    if (input.IndexOf(item) != input.LastIndexOf(item) && !dupe.Contains(item))
+    {
+        dupe.Add(item);
+    }
 }
 
-Console.WriteLine("Now type in a letter for the program to search and count: ");
+Console.WriteLine(string.Join(", ", dupe));
+Console.WriteLine();
 
+// A program returns an array of strings that are unique words found in the argument.
 
+Console.WriteLine("This sentence will remove all duplicate words:");
+string sentence = "to be or not to be";
+Console.WriteLine(sentence);
+string newSentence = string.Join(" ", sentence.Split(' ').Distinct());
+
+Console.WriteLine("New sentence:");
+Console.WriteLine(newSentence);
+Console.WriteLine();
+
+// A program that reverses a provided string 
+
+Console.WriteLine("This sentence will be reversed:");
+string reverseString = "Have you met my cat named tacocat?";
+Console.WriteLine(reverseString);
+char[] reverseArray = reverseString.ToCharArray();
+Array.Reverse(reverseArray);
+Console.WriteLine("The sentence reversed:");
+Console.WriteLine(reverseArray);
+Console.WriteLine();
+
+// A program that finds the longest unbroken word in a string and prints it
+
+string sentence2 = "Within this sentence holds a number of words to find the declared longest.";
+Console.WriteLine(sentence2);
+string[] newSentence2 = sentence2.Split(" ".ToString());
+int longestCalc = newSentence2.Max(w => w.Length);
+string longestString = newSentence2.OrderByDescending(w => w.Length).First();
+Console.WriteLine($"The longest word in the sentence is {longestCalc} letters long and is: {longestString}");
